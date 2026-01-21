234ac794962eb4f122d773f4e31d7cda4cebf35c
Mohammad Al Bast <mohammadbast4@gmail.com>
2026-01-21 13:40:46 +0200

    Refactor app.js to organize imports and middleware setup

--- app.js
+++ app.js
@@ -1,10 +1,28 @@
-import express from 'express';
-import expressEjsLayouts from 'express-ejs-layouts';
-import connectDatabase from './config/database.js';
+import express from "express";
+import connectDatabase from "./src/config/database.js";
+import productRouter from "./src/routers/product.js";
+import expressEjsLayouts from "express-ejs-layouts";
 
 const app = express();
 const PORT = 3000;
-const connectionString = 'mongodb://localhost:27017/library';
+const connectionString = "mongodb://localhost:27017/library";
+
+//middlewares
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("src/public"));
+
+//routers
+app.use("/api", productRouter);
+
+app.use(expressEjsLayouts);
+app.set("view engine", "ejs");
+app.set("views", "./src/views");
+app.set("layout", "./layouts/main");
 
 // Connect to the database
 connectDatabase(connectionString);
+
+app.listen(PORT, () => {
+	console.log(`Server is running on http://localhost:${PORT}`);
+});
