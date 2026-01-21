f414dbd86607763479c4108648f310518f335cf9
Mohammad Al Bast <mohammadbast4@gmail.com>
2026-01-21 12:49:35 +0200

    Refactor product model schema for improved clarity and structure

--- src/models/product.model.js
+++ src/models/product.model.js
@@ -1,1 +1,15 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
+
+const productSchema = new mongoose.Schema(
+	{
+		id: { type: String, required: true, unique: true },
+		name: { type: String, required: true },
+		sku: { type: String, require: true, unique: true },
+		category: { type: String, required: true },
+		price: { type: Number, required: true },
+		quantity: { type: Number, required: true },
+		supplier_id: { type: Number, required: true },
+		description: { type: String, required: false },
+	},
+	{ timestamps: true },
+);
