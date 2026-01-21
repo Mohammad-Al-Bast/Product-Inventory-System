5da05e3113e810034c46a442f0c0e7c994c19df1
Mohammad Al Bast <mohammadbast4@gmail.com>
2026-01-21 12:49:47 +0200

    Add supplier schema definition with validation rules

--- src/models/supplier.model.js
+++ src/models/supplier.model.js
@@ -1,1 +1,18 @@
 import mongoose from 'mongoose';
+
+const supplierSchema = new mongoose.Schema(
+    {
+        id: { type: Number, required: true, unique: true },
+        name: { type: String, required: true },
+        email: { type: String, require: true, unique: true },
+        phone: { type: Number, required: true },
+        website: { type: String, require: false },
+        address: { type: String, require: true},
+        city: { type: String, require: true},
+        state: { type: String, require: true},
+        zip: { type: Number, required: true },
+        country: { type: String, require: true, default: 'Lebanon' },
+        notes: { type: String, required: false },
+    },
+    { timestamps: true },
+);
